c1c9d97fcadc1f861742376e58e2b3e63c32ef25
JCJX <zf19970822@outlook.com>
2017-06-17 02:35:51 +0900

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+
+from urllib.request import urlopen
+from urllib.error import HTTPError,URLError
+from bs4 import BeautifulSoup
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except(HTTPError, URLError) as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read())
+        title = bsObj.body.h1
+    except ArithmeticError as e:
+        return None
+    return title
+title = getTitle("https://avanifull.com/")
+if title == None:
+    print("出错了")
+else:
+    print(title)
+
