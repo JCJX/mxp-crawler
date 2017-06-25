d303ba04c0191f3483e62f7714527d198e197a4e
JCJX <zf19970822@outlook.com>
2017-06-25 15:14:32 +0900

    save amina form mikanani

---
+++ down.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+import csv
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+#获取蜜柑计划上的本季所有番剧
+html=urlopen("http://mikanani.me/")
+bsobj=BeautifulSoup(html)
+animalist=bsobj.findAll("", {"class": "an-text"})
+for name in animalist:
+   print(name.get_text())
+
+
+csvFile= open("../files/jun.cav", 'wt', nameline="", encoding="utf-8")
+writer=csv.writer(csvFile)
+try:
+    for anima in animalist:
+        csvRow=[]
+        for cell in anima.findAll(["td","th"]):
+            csvRow.append(cell.get_text())
+        writer.writerow(csvRow)
+finally:
+    csvFile.close()
+
+
+
--- quicks.py
+++ quicks.py
@@ -10,18 +10,14 @@ from PIL import ImageOps
 def cleanImage(imagePath):
     image =Image.open(imagePath)
     image =image.point(lambda x: 0 if x<143 else 255)
-    borderImage = ImageOps.expand(image,border=20,fill="white")
+    borderImage = ImageOps.expand(image, border=20, fill='white')
     borderImage.save(imagePath)
 
 html =urlopen("http://www.880qp.net/AdminV2/AdminLogin.aspx")
 bsObj =BeautifulSoup(html)
-imageLocation =bsObj.find("img", {"id": "imgyzm"})["src"]
-formBuildId =bsObj.find("input", {"name": "UserName"})["value"]
-captchaSid =bsObj.find("input", {"name": "UserPwd"})["value"]
-captchaToken =bsObj.find("input", {"name": "ValidCode"})["value"]
 
-captchaUrl ="http://www.880qp.net/AdminV2/"+imageLocation
-urlretrieve(captchaUrl,"captcha.jpg")
+captchaUrl ="http://www.880qp.net/AdminV2/CheckCode.aspx"
+urlretrieve(captchaUrl, "captcha.jpg")
 cleanImage("captcha.jpg")
 p = subprocess.Popen(["tesseract", "captcha.jpg", "captcha"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 p.wait()
@@ -31,7 +27,7 @@ captchaResponse = f.read().replace(" ", "").replace("\n", "")
 print("验证码解决方案尝试: "+captchaResponse)
 
 if len(captchaResponse) == 4:
-    params ={"UserName":formBuildId,"UserPwd":captchaSid,"captcha_response":captchaResponse, "name": "Ryan Mitchell", "subject":"I come to seek the Grall", "comment_body[und][0][value]":"...and I am definitely not abot"}
+    params ={'UserName': 'kefu880', 'UserPwd': '880880', "captcha_response":captchaResponse, "name": "Ryan Mitchell", "subject":"I come to seek the Grall", "comment_body[und][0][value]":"...and I am definitely not abot"}
     r=requests.post("http://www.880qp.net/AdminV2/AdminLogin.aspx", date=params)
     responseObj = BeautifulSoup(r.text)
     if responseObj.find("title",{"id": "menubox"}) is not None:
